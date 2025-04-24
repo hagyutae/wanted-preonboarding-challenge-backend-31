b7e74bf81301d5d345841b330dc2c3b987eb644a
hongxeob <sjun2918@naver.com>
2025-04-24 20:52:55 +0900

    refactor: repository 수정

--- ecommerce/src/main/java/com/ecommerce/seller/infrastructure/SellerRepository.java
+++ ecommerce/src/main/java/com/ecommerce/seller/infrastructure/SellerRepository.java
@@ -2,6 +2,8 @@ package com.ecommerce.seller.infrastructure;
 
 import com.ecommerce.seller.domain.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SellerRepository extends JpaRepository<Seller, Long> {
 }
