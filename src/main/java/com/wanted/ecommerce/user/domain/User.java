1965f81955144aa32f2b0800d6ee9c0fb10ced22
Mungi Cheon <c.mungi7421@gmail.com>
2025-04-25 01:09:18 +0900

    [feat] - User Entity 구현

---
+++ src/main/java/com/wanted/ecommerce/user/domain/User.java
@@ -0,0 +1,34 @@
+package com.wanted.ecommerce.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "reviews")
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String avatarUrl;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
