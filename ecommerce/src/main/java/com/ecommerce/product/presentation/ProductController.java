60c6c1178588d78f493656b0ab82fe28bba07f63
hongxeob <sjun2918@naver.com>
2025-04-24 16:16:50 +0900

    feat: 상품 등록 컨트롤러 구현

---
+++ ecommerce/src/main/java/com/ecommerce/product/presentation/ProductController.java
@@ -0,0 +1,27 @@
+package com.ecommerce.product.presentation;
+
+import com.ecommerce.common.response.ApiResponse;
+import com.ecommerce.product.application.ProductService;
+import com.ecommerce.product.application.dto.req.ProductCreateRequest;
+import com.ecommerce.product.application.dto.res.ProductCreatedResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<ProductCreatedResponse>> createProduct(@RequestBody ProductCreateRequest request) {
+        ProductCreatedResponse response = productService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.success(response, "상품이 성공적으로 등록되었습니다."));
+    }
+}
