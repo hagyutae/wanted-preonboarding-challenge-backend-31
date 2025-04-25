dcc758dc4f8a413b5a4787b7f5829ff5c9e1637b
mkhwnag <hmk6264@gmail.com>
2025-04-25 14:40:05 +0900

    init

---
+++ wanted-cqrs/src/main/java/com/mkhwang/wantedcqrs/WantedCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.mkhwang.wantedcqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedCqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedCqrsApplication.class, args);
+	}
+
+}
