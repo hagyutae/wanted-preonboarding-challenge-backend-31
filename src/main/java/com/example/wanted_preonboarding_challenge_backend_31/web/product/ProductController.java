ef74e6964772ecf95bf326215aaed5501a0047ab
ajroot5685 <anjeong8577@naver.com>
2025-04-25 18:13:30 +0900

    feat(product): 수정 API - 컨트롤러

--- src/main/java/com/example/wanted_preonboarding_challenge_backend_31/web/product/ProductController.java
+++ src/main/java/com/example/wanted_preonboarding_challenge_backend_31/web/product/ProductController.java
@@ -5,9 +5,12 @@ import com.example.wanted_preonboarding_challenge_backend_31.common.dto.SuccessR
 import com.example.wanted_preonboarding_challenge_backend_31.web.product.dto.ProductSuccessType;
 import com.example.wanted_preonboarding_challenge_backend_31.web.product.dto.request.ProductCreateReq;
 import com.example.wanted_preonboarding_challenge_backend_31.web.product.dto.response.ProductCreateRes;
+import com.example.wanted_preonboarding_challenge_backend_31.web.product.dto.response.ProductUpdateRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +25,13 @@ public class ProductController {
     @PostMapping
     public SuccessRes<ProductCreateRes> create(@RequestBody @Validated ProductCreateReq productCreateReq) {
         return SuccessRes.of(ProductSuccessType.PRODUCT_CREATE, productService.create(productCreateReq));
+    }
+
+    @PutMapping("/{id}")
+    public SuccessRes<ProductUpdateRes> update(
+            @PathVariable("id") Long productId,
+            @RequestBody @Validated ProductCreateReq productCreateReq
+    ) {
+        return SuccessRes.of(ProductSuccessType.PRODUCT_UPDATE, productService.update(productId, productCreateReq));
     }
 }
--- src/main/java/com/example/wanted_preonboarding_challenge_backend_31/web/product/dto/ProductSuccessType.java
+++ src/main/java/com/example/wanted_preonboarding_challenge_backend_31/web/product/dto/ProductSuccessType.java
@@ -5,7 +5,9 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum ProductSuccessType implements SuccessType {
-    PRODUCT_CREATE("상품이 성공적으로 등록되었습니다.");
+    PRODUCT_CREATE("상품이 성공적으로 등록되었습니다."),
+    PRODUCT_UPDATE("상품이 성공적으로 수정되었습니다."),
+    ;
 
     private final String message;
 
