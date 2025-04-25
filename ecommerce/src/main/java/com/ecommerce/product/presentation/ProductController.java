993e1bedd209f12c29f2c3bfceaf87c62ca342df
hongxeob <sjun2918@naver.com>
2025-04-25 16:54:00 +0900

    refactor: endpoint 수정

--- ecommerce/src/main/java/com/ecommerce/product/presentation/ProductController.java
+++ ecommerce/src/main/java/com/ecommerce/product/presentation/ProductController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
