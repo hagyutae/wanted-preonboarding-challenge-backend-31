adc7b0c3fcac60cc52b0561f739edb3a7ba5cd95
ajroot5685 <anjeong8577@naver.com>
2025-04-25 19:13:10 +0900

    feat(product): 옵션 생성 API - 레포지토리

--- src/main/java/com/example/wanted_preonboarding_challenge_backend_31/domain/repository/product/ProductOptionGroupRepository.java
+++ src/main/java/com/example/wanted_preonboarding_challenge_backend_31/domain/repository/product/ProductOptionGroupRepository.java
@@ -1,9 +1,12 @@
 package com.example.wanted_preonboarding_challenge_backend_31.domain.repository.product;
 
 import com.example.wanted_preonboarding_challenge_backend_31.domain.model.product.ProductOptionGroup;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductOptionGroupRepository extends JpaRepository<ProductOptionGroup, Long> {
 
     void deleteAllByProductId(Long productId);
+
+    Optional<ProductOptionGroup> findByIdAndProductId(Long id, Long productId);
 }
