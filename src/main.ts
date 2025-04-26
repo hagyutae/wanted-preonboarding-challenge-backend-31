16f9c7f0afb3abb8b3a24799bde7dde179736614
NarciSource <jwch11@gmail.com>
2025-04-26 22:18:54 +0900

    chore: Swagger 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
 import { AppModule } from "./module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swagger_config = new DocumentBuilder()
+    .setTitle("API")
+    .setDescription("API 명세서")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swagger_config);
+
+  SwaggerModule.setup("swagger-ui", app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
