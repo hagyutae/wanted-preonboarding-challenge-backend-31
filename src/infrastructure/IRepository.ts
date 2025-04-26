7619c4043ec69435e324628ba5cd0bdaacae1d54
NarciSource <jwch11@gmail.com>
2025-04-26 16:25:40 +0900

    feat: IRepository 인터페이스 생성

---
+++ src/infrastructure/IRepository.ts
@@ -0,0 +1,7 @@
+export default interface IRepository<T> {
+  create(data: T): Promise<T>;
+  findAll(): Promise<T[]>;
+  findById(id: string): Promise<T | null>;
+  update(id: string, data: T): Promise<T>;
+  delete(id: string): Promise<void>;
+}
