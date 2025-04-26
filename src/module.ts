c58e285ae68631c3397bdfc2916d54f05a12d547
NarciSource <jwch11@gmail.com>
2025-04-26 20:55:09 +0900

    fix: 엔터티 변경

--- src/module.ts
+++ src/module.ts
@@ -1,11 +1,11 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
-import Product from "./domain/Product";
 import Product_Service from "./application/Product_Service";
 import typeormConfigProvider from "./infrastructure/provider";
 import ProductRepository from "./infrastructure/ProductRepository";
 import ProductController from "./interfaces/ProductController";
+import { Product } from "./infrastructure/entities/Product.entity";
 
 @Module({
   imports: [typeormConfigProvider, TypeOrmModule.forFeature([Product])],
