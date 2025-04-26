04ed6c0f2f768e4621397cc31f562b0a219d46b7
NarciSource <jwch11@gmail.com>
2025-04-26 17:33:32 +0900

    feat: Product_Service 메소드 응답 구현

--- src/application/Product_Service.ts
+++ src/application/Product_Service.ts
@@ -1,10 +1,19 @@
 import IRepository from "src/infrastructure/IRepository";
+import ResponseDTO from "src/interfaces/ResponseDTO";
 
 export default class Product_Service {
   constructor(private repository: IRepository<any>) {}
 
   async create(data: any) {
-    await this.repository.create(data);
+    const product = await this.repository.create(data);
+
+    const response = {
+      success: true,
+      data: product,
+      message: "상품이 성공적으로 등록되었습니다.",
+    } as ResponseDTO;
+
+    return response;
   }
 
   async getAll() {
@@ -12,15 +21,39 @@ export default class Product_Service {
   }
 
   async getById(id: string) {
-    await this.repository.findById(id);
+    const product = await this.repository.findById(id);
+
+    const response = {
+      success: true,
+      data: product,
+      message: "상품 상세 정보를 성공적으로 조회했습니다.",
+    } as ResponseDTO;
+
+    return response;
   }
 
   async update(id: string, data: any) {
-    await this.repository.update(id, data);
+    const product = await this.repository.update(id, data);
+
+    const response = {
+      success: true,
+      data: product,
+      message: "상품이 성공적으로 수정되었습니다.",
+    } as ResponseDTO;
+
+    return response;
   }
 
   async delete(id: string) {
     await this.repository.delete(id);
+
+    const response = {
+      success: true,
+      data: null,
+      message: "상품이 성공적으로 삭제되었습니다.",
+    } as ResponseDTO;
+
+    return response;
   }
 
   async addOption(id: string, option: any) {
@@ -28,10 +61,18 @@ export default class Product_Service {
 
     const updatedProduct = {
       ...product,
-      option_groups: [...(product?.options_group || []), option],
+      option_groups: [...(product?.option_groups || []), option],
     };
 
     await this.repository.update(id, updatedProduct);
+
+    const response = {
+      success: true,
+      data: updatedProduct,
+      message: "상품 옵션이 성공적으로 추가되었습니다.",
+    } as ResponseDTO;
+
+    return response;
   }
 
   async updateOption(id: string, option: any) {
@@ -39,12 +80,20 @@ export default class Product_Service {
 
     const updatedProduct = {
       ...product,
-      option_groups: product?.options_group.map((opt: any) =>
+      option_groups: product?.option_groups.map((opt: any) =>
         opt.id === option.id ? option : opt,
       ),
     };
 
     await this.repository.update(id, updatedProduct);
+
+    const response = {
+      success: true,
+      data: updatedProduct,
+      message: "상품 옵션이 성공적으로 수정되었습니다.",
+    } as ResponseDTO;
+
+    return response;
   }
 
   async deleteOption(id: string) {
@@ -52,10 +101,18 @@ export default class Product_Service {
 
     const updatedProduct = {
       ...product,
-      option_groups: product?.options_group.filter((opt: any) => opt.id !== id),
+      option_groups: product?.option_groups.filter((opt: any) => opt.id !== id),
     };
 
     await this.repository.update(id, updatedProduct);
+
+    const response = {
+      success: true,
+      data: null,
+      message: "상품 옵션이 성공적으로 삭제되었습니다.",
+    } as ResponseDTO;
+
+    return response;
   }
 
   addImage(image: any) {
