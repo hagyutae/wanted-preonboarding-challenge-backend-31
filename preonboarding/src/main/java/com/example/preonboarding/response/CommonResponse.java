e348fa5bd958ca90f3a3cf65e43a5b5996cf8199
yangtaeyoung93 <ngj100@naver.com>
2025-04-26 22:10:43 +0900

    feat : Add CommonResponse, ErrorResponse

---
+++ preonboarding/src/main/java/com/example/preonboarding/response/CommonResponse.java
@@ -0,0 +1,36 @@
+package com.example.preonboarding.response;
+
+import com.example.preonboarding.response.error.ErrorResponse;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CommonResponse<T> {
+    private boolean success;
+    private T data;
+    private String message;
+    private ErrorResponse error;
+
+    public CommonResponse(boolean success, T data, String message) {
+        this.success = success;
+        this.data = data;
+        this.message = message;
+    }
+
+    public CommonResponse(boolean success, ErrorResponse error) {
+        this.success = success;
+        this.error = error;
+    }
+
+
+    public static <T> CommonResponse<CommonResponse> success(T data) {
+        return new CommonResponse(true, data, "요청이 성공적으로 처리되었습니다.");
+    }
+
+    public static CommonResponse error(ErrorResponse error) {
+        return new CommonResponse(false, error);
+    }
+}
