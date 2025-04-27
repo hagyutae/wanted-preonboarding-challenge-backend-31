2acc6ed51a819e04eedbadd9bbaea0e044311c66
NarciSource <jwch11@gmail.com>
2025-04-27 11:07:49 +0900

    feat: jest 커버리지 제외 패턴 추가

--- jest.config.ts
+++ jest.config.ts
@@ -20,9 +20,12 @@ const config: Config = {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  coveragePathIgnorePatterns: [
+    "\\\\node_modules\\\\",
+    "src/domain",
+    "src/infrastructure/entities",
+    "src/interfaces/dto",
+  ],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
