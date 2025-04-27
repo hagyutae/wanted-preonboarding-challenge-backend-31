9830e302408bcb438d7243ace1db94981c211706
NarciSource <jwch11@gmail.com>
2025-04-28 08:58:57 +0900

    test: 카테고리 트리 구조로 반환함수 테스트

--- src/application/CategoryService.test.ts
+++ src/application/CategoryService.test.ts
@@ -71,4 +71,43 @@ describe("CategoryService", () => {
       expect(result).toEqual([]);
     });
   });
+
+  describe("getAllCategoriesAsTree", () => {
+    it("카테고리를 트리 구조로 반환", async () => {
+      const mockCategories = [
+        { id: 1, name: "대분류1" },
+        { id: 2, name: "중분류1", parent: { id: 1 } },
+        { id: 3, name: "소분류1", parent: { id: 2 } },
+      ] as CategoryEntity[];
+
+      mockEntityManager.find.mockResolvedValue(mockCategories);
+
+      const result = await service.getAllCategoriesAsTree();
+
+      expect(mockEntityManager.find).toHaveBeenCalledWith(CategoryEntity, {
+        relations: ["parent"],
+      });
+      expect(result).toEqual([
+        {
+          id: 1,
+          name: "대분류1",
+          children: [
+            {
+              id: 2,
+              name: "중분류1",
+              parent: { id: 1 },
+              children: [
+                {
+                  id: 3,
+                  name: "소분류1",
+                  parent: { id: 2 },
+                  children: [],
+                },
+              ],
+            },
+          ],
+        },
+      ]);
+    });
+  });
 });
