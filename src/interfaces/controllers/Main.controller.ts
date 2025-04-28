460ba6da279130b2a4a908aedad6e43ae3ffb6d6
NarciSource <jwch11@gmail.com>
2025-04-28 21:38:20 +0900

    feat: MainController 구현

---
+++ src/interfaces/controllers/Main.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get } from "@nestjs/common";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
+
+import { ResponseDTO } from "../dto";
+import MainService from "src/application/MainService";
+
+@ApiTags("메인")
+@Controller("main")
+export default class MainController {
+  constructor(private readonly service: MainService) {}
+
+  @ApiOperation({ summary: "메인 페이지용 상품 목록" })
+  @Get()
+  async getMainProducts(): Promise<ResponseDTO> {
+    const new_products = await this.service.getNewProducts();
+
+    const popular_products = await this.service.getPopularProducts();
+
+    const featured_categories = await this.service.getFeaturedCategories();
+
+    return {
+      success: true,
+      data: {
+        new_products,
+        popular_products,
+        featured_categories,
+      },
+      message: "메인 페이지 상품 목록을 성공적으로 조회했습니다.",
+    };
+  }
+}
--- src/interfaces/controllers/index.ts
+++ src/interfaces/controllers/index.ts
@@ -2,3 +2,4 @@ export { default as ProductController } from "./Product.controller";
 export { default as ProductOptionsController } from "./Product_Options.controller";
 export { default as CategoryController } from "./Category.controller";
 export { default as ReviewController } from "./Review.controller";
+export { default as MainController } from "./Main.controller";
