2612007e9798a508f28281b9e13feec9ed1073b9
1117mg <lee_yerim@naver.com>
2025-04-28 18:12:05 +0900

    init: initialize Spring Boot project

---
+++ src/main/java/com/shopping/mall/challenge1117/Challenge1117Application.java
@@ -0,0 +1,13 @@
+package com.shopping.mall.challenge1117;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge1117Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Challenge1117Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/shopping/mall/challenge1117/Challenge1117ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.mall.challenge1117;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge1117ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
