300d70f5c9e058fa7686bcef72cb59049ead7087
NarciSource <jwch11@gmail.com>
2025-04-28 23:48:10 +0900

    feat: 컨트롤러, 서비스 DI

--- src/module.ts
+++ src/module.ts
@@ -1,16 +1,25 @@
 import { Module } from "@nestjs/common";
 
-import { ProductService, ProductOptionsService, CategoryService } from "./application";
+import { ProductService, ProductOptionsService, CategoryService, MainService } from "./application";
 import typeormConfigProvider from "./infrastructure/provider";
 import {
-  CategoryController,
   ProductController,
   ProductOptionsController,
+  ReviewController,
+  CategoryController,
+  MainController,
 } from "./interfaces/controllers";
+import ReviewService from "./application/ReviewService";
 
 @Module({
   imports: [typeormConfigProvider],
-  providers: [ProductService, ProductOptionsService, CategoryService],
-  controllers: [ProductController, ProductOptionsController, CategoryController],
+  providers: [ProductService, ProductOptionsService, CategoryService, ReviewService, MainService],
+  controllers: [
+    ProductController,
+    ProductOptionsController,
+    CategoryController,
+    ReviewController,
+    MainController,
+  ],
 })
 export class AppModule {}
