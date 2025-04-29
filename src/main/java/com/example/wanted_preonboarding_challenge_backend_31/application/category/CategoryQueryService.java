67ec541721313e78888e9b506f0e8dbbf0857992
ajroot5685 <anjeong8577@naver.com>
2025-04-29 15:57:48 +0900

    feat(category): 특정 카테고리 상품 목록 조회 - 서비스

--- src/main/java/com/example/wanted_preonboarding_challenge_backend_31/application/category/CategoryQueryService.java
+++ src/main/java/com/example/wanted_preonboarding_challenge_backend_31/application/category/CategoryQueryService.java
@@ -5,6 +5,8 @@ import com.example.wanted_preonboarding_challenge_backend_31.common.exception.Co
 import com.example.wanted_preonboarding_challenge_backend_31.common.exception.CustomException;
 import com.example.wanted_preonboarding_challenge_backend_31.domain.model.category.Category;
 import com.example.wanted_preonboarding_challenge_backend_31.domain.repository.category.CategoryRepository;
+import com.example.wanted_preonboarding_challenge_backend_31.shared.dto.category.CategoryDetailDto;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,5 +22,24 @@ public class CategoryQueryService {
         return categoryRepository.findById(id)
                 .orElseThrow(() -> new CustomException(
                         ErrorInfo.of(CommonErrorType.RESOURCE_NOT_FOUND, "요청한 카테고리를 찾을 수 없습니다, ID:" + id)));
+    }
+
+    public CategoryDetailDto getCategoryDetailById(Long id) {
+        Category category = getById(id);
+        return CategoryDetailDto.from(category);
+    }
+
+    /**
+     * includeSubcategories에 따라 하위 카테고리를 포함하거나 포함하지 않고 Id 리스트를 반환한다.<br> 부모 카테고리 Id는 반드시 포함된다.
+     *
+     * @param categoryId           부모 카테고리 Id
+     * @param includeSubcategories 하위 카테고리 포함 여부 - 기본값 : TRUE
+     * @return 카테고리 Id 리스트
+     */
+    public List<Long> getSubcategoryIds(Long categoryId, Boolean includeSubcategories) {
+        if (Boolean.FALSE.equals(includeSubcategories)) {
+            return List.of(categoryId);
+        }
+        return categoryRepository.findAllChildCategoryIds(categoryId);
     }
 }
---
+++ src/main/java/com/example/wanted_preonboarding_challenge_backend_31/application/category/CategoryService.java
@@ -0,0 +1,33 @@
+package com.example.wanted_preonboarding_challenge_backend_31.application.category;
+
+import com.example.wanted_preonboarding_challenge_backend_31.application.product.ProductComplexQueryService;
+import com.example.wanted_preonboarding_challenge_backend_31.shared.dto.category.CategoryDetailDto;
+import com.example.wanted_preonboarding_challenge_backend_31.shared.dto.pagination.PaginationReq;
+import com.example.wanted_preonboarding_challenge_backend_31.web.category.dto.request.CategoryProductReq;
+import com.example.wanted_preonboarding_challenge_backend_31.web.category.dto.response.CategoryProductRes;
+import com.example.wanted_preonboarding_challenge_backend_31.web.product.dto.request.ProductSearchReq;
+import com.example.wanted_preonboarding_challenge_backend_31.web.product.dto.response.ProductSearchRes;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final CategoryQueryService categoryQueryService;
+    private final ProductComplexQueryService productComplexQueryService;
+
+    public CategoryProductRes searchProduct(PaginationReq paginationReq, Long categoryId, CategoryProductReq req) {
+        CategoryDetailDto categoryDetail = categoryQueryService.getCategoryDetailById(categoryId);
+
+        List<Long> categoryIds = categoryQueryService.getSubcategoryIds(categoryId, req.includeSubcategories());
+        ProductSearchReq productSearchReq = new ProductSearchReq(req.sort(), null, null, null, categoryIds, null, null,
+                null, null);
+        ProductSearchRes productSearchRes = productComplexQueryService.searchProducts(paginationReq, productSearchReq);
+
+        return new CategoryProductRes(categoryDetail, productSearchRes.items(), productSearchRes.pagination());
+    }
+}
