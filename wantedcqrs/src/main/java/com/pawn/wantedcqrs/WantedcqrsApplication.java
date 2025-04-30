399ce824da03a73142f15175454d72fc8ea7a365
pawn <devrunner21@gmail.com>
2025-04-30 22:01:01 +0900

    project init

---
+++ wantedcqrs/src/main/java/com/pawn/wantedcqrs/WantedcqrsApplication.java
@@ -0,0 +1,13 @@
+package com.pawn.wantedcqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedcqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedcqrsApplication.class, args);
+	}
+
+}
---
+++ wantedcqrs/src/test/java/com/pawn/wantedcqrs/WantedcqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawn.wantedcqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedcqrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
