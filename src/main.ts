afff1e803eb4c00b199928de1e98dd9bf93acb72
NarciSource <jwch11@gmail.com>
2025-05-01 21:05:50 +0900

    feat: 스웨거 UI에 인증 유지 옵션 추가

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,21 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, swagger_config);
 
-  SwaggerModule.setup("swagger-ui", app, document);
+  // 스웨거 UI 설정
+  SwaggerModule.setup("swagger-ui", app, document, {
+    /**
+     * 제거 필요
+     * 인증, 인가 미구현으로 자동 통과되도록 설정
+     *
+     **/
+    swaggerOptions: {
+      persistAuthorization: true,
+      requestInterceptor: (req: { headers: { [x: string]: string } }) => {
+        req.headers["Authorization"] = "Bearer Token";
+        return req;
+      },
+    },
+  });
 
   // 앱 실행
   await app.listen(process.env.PORT ?? 3000);
