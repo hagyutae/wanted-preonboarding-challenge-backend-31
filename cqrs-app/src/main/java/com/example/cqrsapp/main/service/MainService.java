62d4cf6f6b953df8d662fa88e5de5c924e26fc84
hanseungyeoun <aksla1122@naver.com>
2025-05-05 17:23:18 +0900

    fix: MainService에 @Transactional 어너테이션 추가

--- cqrs-app/src/main/java/com/example/cqrsapp/main/service/MainService.java
+++ cqrs-app/src/main/java/com/example/cqrsapp/main/service/MainService.java
@@ -4,6 +4,7 @@ import com.example.cqrsapp.main.dto.MainResponse;
 import com.example.cqrsapp.main.repository.MainRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,6 +12,7 @@ public class MainService {
 
     public final MainRepository mainRepository;
 
+    @Transactional(readOnly = true)
     public MainResponse getMainPageContent() {
         return MainResponse.builder()
                 .newProducts(mainRepository.findNewProducts())
