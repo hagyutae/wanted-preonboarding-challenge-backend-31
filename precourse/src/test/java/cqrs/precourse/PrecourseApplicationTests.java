9ce65683187ebe4b1c47706446cbfc3f860e1d84
EunSeo0117 <gmjea023@gmail.com>
2025-05-06 00:46:41 +0900

    init commit

---
+++ precourse/src/test/java/cqrs/precourse/PrecourseApplicationTests.java
@@ -0,0 +1,13 @@
+package cqrs.precourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
