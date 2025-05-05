ddb63b692433849f256b4b3f9b2116b64dcc2b8c
Mungi Cheon <c.mungi7421@gmail.com>
2025-05-05 23:14:15 +0900

    [feat] - Category API 추가

--- src/main/java/com/wanted/ecommerce/category/repository/CategoryRepository.java
+++ src/main/java/com/wanted/ecommerce/category/repository/CategoryRepository.java
@@ -1,8 +1,13 @@
 package com.wanted.ecommerce.category.repository;
 
 import com.wanted.ecommerce.category.domain.Category;
+import com.wanted.ecommerce.product.repository.ProductSearchRepository;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long>, ProductSearchRepository {
 
+    List<Category> findAllByLevel(Integer level);
+
+    List<Category> findAllByParentId(Long parentId);
 }
--- src/main/java/com/wanted/ecommerce/category/service/CategoryService.java
+++ src/main/java/com/wanted/ecommerce/category/service/CategoryService.java
@@ -1,8 +1,12 @@
 package com.wanted.ecommerce.category.service;
 
 import com.wanted.ecommerce.category.domain.Category;
+import com.wanted.ecommerce.category.dto.response.CategoriesResponse;
+import java.util.List;
 
-public interface CategoryService  {
+public interface CategoryService {
 
     Category getCategoryById(Long id);
+
+    List<CategoriesResponse> getAllCategoryByLevel(int level);
 }
