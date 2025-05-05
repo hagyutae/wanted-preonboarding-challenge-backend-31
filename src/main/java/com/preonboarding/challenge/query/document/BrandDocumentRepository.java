8e580536fd684a567d889b6a10cf29047a3fd321
hagyutae <gted221@gmail.com>
2025-05-06 00:10:47 +0900

    mongodb query model 동기화 추가; product 외 모델들

---
+++ src/main/java/com/preonboarding/challenge/query/document/BrandDocumentRepository.java
@@ -0,0 +1,8 @@
+package com.preonboarding.challenge.query.document;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandDocumentRepository extends MongoRepository<BrandDocument, Long> {
+}
