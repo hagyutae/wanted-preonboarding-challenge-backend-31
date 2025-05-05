1a09fef8558b391a8cf88578cb558dc1a28094d4
hanseungyeoun <aksla1122@naver.com>
2025-05-05 15:27:10 +0900

    init

---
+++ cqrs-app/src/main/java/com/example/cqrsapp/CqrsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cqrsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsAppApplication.class, args);
+	}
+
+}
