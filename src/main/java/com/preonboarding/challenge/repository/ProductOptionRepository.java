356f8484dec5e31f0f630ef88c7d9c0052e4f9d3
hagyutae <gted221@gmail.com>
2025-05-05 23:56:32 +0900

    mongodb query model 동기화 추가

--- src/main/java/com/preonboarding/challenge/repository/ProductOptionRepository.java
+++ src/main/java/com/preonboarding/challenge/repository/ProductOptionRepository.java
@@ -1,7 +1,30 @@
 package com.preonboarding.challenge.repository;
 
 import com.preonboarding.challenge.entity.ProductOption;
+import com.preonboarding.challenge.repository.projection.ProductOptionProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
+
+    @Query("SELECT o.id as id, o.name as name, o.additionalPrice as additionalPrice, " +
+            "o.sku as sku, o.stock as stock, o.displayOrder as displayOrder, og.id as optionGroupId " +
+            "FROM ProductOption o " +
+            "JOIN o.optionGroup og " +
+            "WHERE og.id = :optionGroupId")
+    List<ProductOptionProjection> findOptionsByOptionGroupId(@Param("optionGroupId") Long optionGroupId);
+
+    @Query("SELECT o.id as id, o.name as name, o.additionalPrice as additionalPrice, " +
+            "o.sku as sku, o.stock as stock, o.displayOrder as displayOrder, og.id as optionGroupId " +
+            "FROM ProductOption o " +
+            "JOIN o.optionGroup og " +
+            "JOIN og.product p " +
+            "WHERE p.id = :productId")
+    List<ProductOptionProjection> findOptionsByProductId(@Param("productId") Long productId);
+
+    @Query("SELECT o.optionGroup.product.id FROM ProductOption o WHERE o.id = :optionId")
+    Long findProductIdByOptionId(@Param("optionId") Long optionId);
 }
---
+++ src/main/java/com/preonboarding/challenge/repository/projection/OptionGroupWithProductProjection.java
@@ -0,0 +1,8 @@
+package com.preonboarding.challenge.repository.projection;
+
+public interface OptionGroupWithProductProjection {
+    Long getId();
+    String getName();
+    Integer getDisplayOrder();
+    Long getProductId();
+}
