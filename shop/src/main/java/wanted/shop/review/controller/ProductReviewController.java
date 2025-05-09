6904ae56121200f3e046818ab0825352f78990e9
Heesom <heesom.hs@gmail.com>
2025-05-09 11:00:06 +0900

    refac: 리뷰 조회 컨트롤러 다른 클래스로 분리

---
+++ shop/src/main/java/wanted/shop/review/controller/ProductReviewController.java
@@ -0,0 +1,30 @@
+package wanted.shop.review.controller;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import wanted.shop.common.api.SuccessResponse;
+import wanted.shop.product.domain.ProductId;
+import wanted.shop.review.domain.query.ReviewPageRequest;
+import wanted.shop.review.dto.ReviewListResponse;
+import wanted.shop.review.service.ReviewService;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/products")
+public class ProductReviewController {
+
+    private ReviewService reviewService;
+
+    @GetMapping("/{productId}/reviews")
+    public SuccessResponse<ReviewListResponse> getReviewsByProductId(
+            @ModelAttribute @Valid ReviewPageRequest request,
+            @PathVariable long productId
+    ) {
+        System.out.println(request.toString());
+        ReviewListResponse response = reviewService.getReviewsByProductId(new ProductId(productId), request);
+        return new SuccessResponse<>(response);
+    }
+
+}
+
