0c3e0ff73c61fa8abed188922bc81412f883c125
EunSeo0117 <gmjea023@gmail.com>
2025-05-09 18:46:05 +0900

    fix: 직렬화 역직렬화 오류 수정

---
+++ precourse/src/main/java/cqrs/precourse/config/JacksonConfig.java
@@ -0,0 +1,20 @@
+package cqrs.precourse.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // ISO-8601 포맷 출력
+        return objectMapper;
+    }
+}
